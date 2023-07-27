bbe122cc5976eb1df47e8cf6312c0499fa036ba9
Yuliya Labutina <labutinayuliya3721@gmail.com>
2023-07-27 13:03:29 +0500

    main

---
+++ script.js
@@ -0,0 +1,73 @@
+//смена темы
+let page=document.querySelector('.page');
+
+let darkThemeButton=document.querySelector('.theme-button-dark');
+let lightThemeButton=document.querySelector('.theme-button-light');
+
+darkThemeButton.onclick = function(){
+page.classList.add('dark');
+lightThemeButton.classList.remove('active')
+darkThemeButton.classList.add('active')
+}
+
+lightThemeButton.onclick = function(){
+    page.classList.remove('dark');
+    lightThemeButton.classList.add('active')
+    darkThemeButton.classList.remove('active')  
+}
+//смена шрифта
+let serifFontButton = document.querySelector('.font-button-serif');
+let sansSerifFontButton=document.querySelector('.font-button-sans-serif');
+
+serifFontButton.onclick = function(){
+  page.classList.add('serif');
+serifFontButton.classList.add('active')
+sansSerifFontButton.classList.remove('active')  
+}
+
+sansSerifFontButton.onclick = function(){
+    page.classList.remove('serif');
+    serifFontButton.classList.remove('active')
+    sansSerifFontButton.classList.add('active') 
+}
+
+//разворачиваем статьи
+//нашли коллекцию со статьями
+/*let blogArticles=document.querySelectorAll('.blog-article');
+for(let blogArticle of blogArticles ){
+    let moreButton=blogArticle.querySelector('.more')
+moreButton.onclick=function(){
+   blogArticle.classList.remove('short') 
+}
+}*/
+
+//переключение раскладки карточек
+let cardButtonGrid=document.querySelector('.card-view-button-grid');
+let cardButtonList=document.querySelector('.card-view-button-list');
+let cardsList=document.querySelector('.cards');
+
+cardButtonList.onclick=function(){
+    cardsList.classList.add('list')
+cardButtonGrid.classList.remove('active')
+cardButtonList.classList.add('active')
+}
+
+cardButtonGrid.onclick=function(){
+    cardsList.classList.remove('list')
+    cardButtonGrid.classList.add('active')
+    cardButtonList.classList.remove('active')  
+}
+
+//галлерея
+let activePhoto=document.querySelector('.active-photo')
+
+let previewList=document.querySelectorAll('.preview-list a')
+for(let activeImage of previewList ){
+
+activeImage.onclick=function(evt){
+    activePhoto.src=activeImage.href
+    evt.preventDefault() 
+    let currentActive=document.querySelector('.preview-list a.active-item')
+currentActive.classList.remove('active-item')
+activeImage.classList.add('active-item')
+}}
